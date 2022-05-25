37879b848883d9fae38a582e7a3b1cdacf553cac
HarishB167 <harishbb168@gmail.com>
2022-05-25 23:27:42 +0530

    Delegates completed

---
+++ Delegates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filter = new PhotoFilters();
+            Action<Photo> filterHandler = filter.ApplyBrightness;
+            filterHandler += filter.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+
+            processor.Process("image.jpg", filterHandler);
+        }
+
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRedEye");
+        }
+    }
+}
