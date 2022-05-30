6e76a251bd89ed65d89ba90ee07080ee6aaf0b55
HarishB167 <harishbb168@gmail.com>
2022-05-30 21:39:47 +0530

    Nullable Types : 7 : completed

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime? date = null;
+            DateTime date2 = date ?? DateTime.Today;
+
+            Console.WriteLine(date2);
+            
+        }
+    }
+}
