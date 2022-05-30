1b75866997159fde1aa67f0cc963707311784f93
HarishB167 <harishbb168@gmail.com>
2022-05-30 21:17:41 +0530

    LINQ : 6 : completed

---
+++ LINQ/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+
+            var pagedBooks = books.Skip(2).Take(3);
+
+            foreach (var book in pagedBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+            Console.WriteLine(books.Max(b => b.Price));
+        }
+    }
+}
