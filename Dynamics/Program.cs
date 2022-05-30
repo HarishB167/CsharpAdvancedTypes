b9c4b6189967ffc3a4070f0ab5688a77b03ef293
HarishB167 <harishbb168@gmail.com>
2022-05-30 22:09:54 +0530

    Dynamics : 8 : completed

---
+++ Dynamics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic a = 10;
+            dynamic b = 10;
+            var c = a + b;
+        }
+    }
+}
