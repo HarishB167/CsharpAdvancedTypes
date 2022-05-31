284895a94d26f1f1513a6e1bc28c0b161238df70
HarishB167 <harishbb168@gmail.com>
2022-05-31 08:41:50 +0530

    Exception Handling : 9 : completed

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var api = new YoutubeApi();
+                var videos = api.GetVideos("harish");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/YoutubeApi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public class Video 
+    {
+    }
+
+    public class YoutubeApi
+    {
+        public List<Video> GetVideos(string user)
+        {
+            try
+            {
+                throw new Exception("Oops some low level Youtube Error.");
+            }
+            catch (Exception ex)
+            {
+
+                throw new YoutubeException("Could not fetch the videos from Youtube.", ex);
+            }
+
+            return new List<Video>();
+        }
+    }
+}
